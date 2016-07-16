8b2d120201a77254eadddc7508ca1bd7b4c514b2
Dave Wasmer <davewasmer@gmail.com>
2016-07-15 21:52:26 -0600

    scaffold server mock

--- mirage/config.js
+++ mirage/config.js
@@ -1,26 +1,24 @@
 export default function() {
 
-  // These comments are here to help you get started. Feel free to delete them.
+  this.get('/tips');
+  this.post('/tips');
+  this.get('/tips/:id');
+  this.patch('/tips/:id');
+  this.delete('/tips/:id');
 
-  /*
-    Config (with defaults).
+  this.get('/users');
+  this.post('/users');
+  this.get('/users/:id');
+  this.patch('/users/:id');
 
-    Note: these only affect routes defined *after* them!
-  */
+  this.get('/votes');
+  this.post('/votes');
+  this.get('/votes/:id');
+  this.delete('/votes/:id');
 
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  this.get('/confirmations');
+  this.post('/confirmations');
+  this.get('/confirmations/:id');
+  this.delete('/confirmations/:id');
 
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
-  */
 }
